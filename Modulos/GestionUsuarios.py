8da5e578182a5aebc353ab3bd397a2a96af6842b
kiloa12 <sancg503@gmail.com>
2024-11-07 09:17:32 -0500

    feat: :white_check_mark: Menu usuarios

---
+++ Modulos/GestionUsuarios.py
@@ -0,0 +1,16 @@
+def MenuUsuarios():
+    print("""
+        --------------------------------
+        -------- MENU USUARIOS ---------
+        (1)Crear Usuario
+        (2)Eliminar un Usuario
+        (3)Lista de Usuario
+          """)
+    op = input("Ingrese Opcion Deseada --> ")
+    match op:
+        case "1":
+            pass
+        case "2":
+            pass
+        case "3":
+            pass
