c1fbbb18b7e556dd8ab03833261ecf92906cfe6d
kiloa12 <sancg503@gmail.com>
2024-11-07 09:34:49 -0500

    feat: :closed_lock_with_key: funciones

--- Modulos/GestionUsuarios.py
+++ Modulos/GestionUsuarios.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 def MenuUsuarios():
     print("""
         --------------------------------
@@ -14,3 +17,16 @@ def MenuUsuarios():
             pass
         case "3":
             pass
+###################################################################################
+def guardarDatos(usuarios,nombre_archivo = 'usuarios.json'):
+    with open(nombre_archivo,"a") as archivo:
+        json.dump(usuarios, archivo, indent=4)
+
+###################################################################################3
+def cargarDatos(nombre_archivo='usuarios.json'):
+    if os.path.exists(nombre_archivo):
+        with open(nombre_archivo, 'r') as archivo:
+            return json.load(archivo)
+    else:
+        return []
+    
